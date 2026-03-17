aeb3c2457585a6fa4d9e3a5832a01f50ac38b983
VijayAnand33 <vijayanand8208@gmail.com>
2026-03-17 15:02:44 +0530

    Week 1 Problem Statements

---
+++ Weekly-Assignment/src/FlashSale.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class FlashSale {
+    static HashMap<String, Integer> stock = new HashMap<>();
+    static Queue<Integer> waitingList = new LinkedList<>();
+
+    public static void purchaseItem(String product, int userId) {
+        int count = stock.getOrDefault(product, 0);
+
+        if (count > 0) {
+            stock.put(product, count - 1);
+            System.out.println("Success. Remaining: " + (count - 1));
+        } else {
+            waitingList.add(userId);
+            System.out.println("Added to waiting list");
+        }
+    }
+
+    public static void main(String[] args) {
+        stock.put("IPHONE15", 2);
+
+        purchaseItem("IPHONE15", 1);
+        purchaseItem("IPHONE15", 2);
+        purchaseItem("IPHONE15", 3);
+    }
+}
---
+++ Weekly-Assignment/src/PlagiarismDetector.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class PlagiarismDetector {
+
+    public static Set<String> getNGrams(String text, int n) {
+        String[] words = text.split(" ");
+        Set<String> grams = new HashSet<>();
+
+        for (int i = 0; i <= words.length - n; i++) {
+            String gram = "";
+            for (int j = 0; j < n; j++)
+                gram += words[i + j] + " ";
+            grams.add(gram.trim());
+        }
+        return grams;
+    }
+
+    public static void main(String[] args) {
+        String doc1 = "java is easy to learn programming language";
+        String doc2 = "java is easy to learn coding language";
+
+        Set<String> g1 = getNGrams(doc1, 3);
+        Set<String> g2 = getNGrams(doc2, 3);
+
+        g1.retainAll(g2);
+
+        System.out.println("Matching n-grams: " + g1.size());
+    }
+}
